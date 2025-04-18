07b544d4f49b24145ac84dba386f612f092df9d9
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2025-04-18 13:50:55 -0400

    Update speed_publisher.hpp

--- headers/speed_publisher.hpp
+++ headers/speed_publisher.hpp
@@ -1,1 +1,26 @@
+// speed_publisher.hpp
+#pragma once
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/float32.hpp>
+
+class SpeedPublisher : public rclcpp::Node
+{
+public:
+  SpeedPublisher()
+  : Node("speed_publisher")
+  {
+    speed_pub_ = create_publisher<std_msgs::msg::Float32>("speed_cmd", 10);
+  }
+
+  /// Call this from your FSM with the speed you decided (m/s)
+  void publish(float v)
+  {
+    std_msgs::msg::Float32 msg;
+    msg.data = v;
+    speed_pub_->publish(msg);
+  }
+
+private:
+  rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr speed_pub_;
+};
 
