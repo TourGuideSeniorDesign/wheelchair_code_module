75c284dff0e936c73f2051124d291368e4e05e35
Angela Busheska <64707998+Angelaangie-ai@users.noreply.github.com>
2025-05-08 10:23:47 -0400

    Create obstacle_publisher_heading.hpp

---
+++ headers/obstacle_publisher_heading.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+
+class ObstaclePublisher : public rclcpp::Node
+{
+public:
+  ObstaclePublisher();
+
+private:
+  /* subscriber */
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr cloud_sub_;
+
+  /* publishers */
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr obstacle_flag_pub_;
+
+  // clear/blocked flags used by the motion controller
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr front_clear_pub_;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr back_clear_pub_;
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr left_turn_clear_pub_;   
+  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr right_turn_clear_pub_;  
+
+  // diagnostic visuals
+  rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr    obstacles_cloud_pub_;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr markers_pub_;
+
+
+  void cloudCallback(const sensor_msgs::msg::PointCloud2::SharedPtr msg);
+};
