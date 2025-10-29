c961301423a26ba77be26c0ce5debef2a1553a94
alexanderlindquis <143742409+alexanderlindquis@users.noreply.github.com>
2025-10-29 13:18:19 -0400

    Add files via upload

---
+++ headers/temp_monitor.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Robbie on 4/21/25.
+//
+
+#ifndef WHEELCHAIR_CODE_MODULE_TEMP_MONITOR_H
+#define WHEELCHAIR_CODE_MODULE_TEMP_MONITOR_H
+
+#include <string>
+
+std::string findCpuThermalZone();
+
+double readTemperature(const std::string& tempPath);
+
+#endif //WHEELCHAIR_CODE_MODULE_TEMP_MONITOR_H
